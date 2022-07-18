7dfacd2e3d042fa409cb524fdf74eb78a319ae66
Max Kennedy <maxken512@gmail.com>
2022-07-18 19:46:07 -0400

    header changes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,37 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import { Toolbar } from "@mui/material";
-import { Avatar, Box, Typography } from "@material-ui/core";
+import { Avatar, Button, Typography, Box } from "@material-ui/core";
 import img from "C:/Users/Max/learning/src/components/header/IMG_0813-3.JPG";
 import { Container } from "@mui/system";
 
 function Header() {
+  const [content, setContent] = useState("WELCOME");
+
   return (
     <header>
       <AppBar position="static" alignItems="right">
         <Container maxWidth="xl">
           <Toolbar disableGutters>
             <Avatar
-              sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+              alignItems="left"
+              sx={{
+                display: { xs: "none", md: "flex" },
+                mr: 1,
+              }}
               alt="Max Kennedy"
               src={img}
             />
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              Maxwell Kennedy
-            </Typography>
+            <Box>
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                Maxwell Kennedy
+              </Typography>
+            </Box>
+            <Container>
+              <Button
+                color="inherit"
+                onClick={() =>
+                  setContent(
+                    "My name is Max Kennedy, I am a bootcamp graduate specializing in front-end web development"
+                  )
+                }
+              >
+                About Me
+              </Button>
+              <Button color="inherit">Contact</Button>
+              <Button color="inherit">Projects</Button>
+            </Container>
           </Toolbar>
         </Container>
       </AppBar>
